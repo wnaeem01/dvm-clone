a6d661099418738474c0a5110376b2e7b7463ecb
Waleed Naeem <wnaeem@charitag.com>
2025-07-18 18:06:18 +0500

    layout refactored

--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,33 @@
-import Features from './components/features'
-import Mainanimator from './components/mainanimator'
-import Bottomheader from './layouts/bottomheader'
-import Header from './layouts/header'
-import Sidebar from './layouts/siderbar'
-import Topheader from './layouts/topheader'
-import { getCategories } from '../app/api/sidebar'; 
-import Hotsellingproducts from './components/hotsellingproducts'
-import Dealsbanner from './components/dealsbanner'
-import Featuredproducts from './components/featuredproducts'
-import Eventdata from './components/eventdata'
-import Vetproducts from './components/vetproducts'
-import Vertinarypractice from './components/vertinaryprac'
-import Vetpetgrid from './components/vetpetgrid'
-import Preferredvendors from './components/preferredvendors'
-import Footer from './layouts/footer'
+import Mainanimator from "./components/mainanimator";
+import Features from "./components/features";
+import Hotsellingproducts from "./components/hotsellingproducts";
+import Dealsbanner from "./components/dealsbanner";
+import Featuredproducts from "./components/featuredproducts";
+import Eventdata from "./components/eventdata";
+import Vetproducts from "./components/vetproducts";
+import Vertinarypractice from "./components/vertinaryprac";
+import Vetpetgrid from "./components/vetpetgrid";
+import Preferredvendors from "./components/preferredvendors";
 
-const categories = await getCategories() || [];
-const page = () => {
-  
+export default function HomePage() {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <Sidebar categories={categories}/>
-      <Topheader/>
-      <Header/>
-      <Bottomheader/>
-      <Mainanimator/>
-      <Features/>
+    <>
+      <Mainanimator />
+      <Features />
       <div className="container mx-auto px-6">
         <Hotsellingproducts />
       </div>
-      <div className='px-62 py-20'>
+      <div className="px-62 py-20">
         <Dealsbanner />
       </div>
       <div className="container mx-auto px-6 mt-20">
-        <Featuredproducts/>
+        <Featuredproducts />
       </div>
-        <div className="my-40 mx-65">
-          <img
-            src="assets/s3.png"
-            alt=""
-            className="w-full h-auto object-cover"
-          />
-        </div>
-        <Eventdata />
-        <Vetproducts />
-
+      <div className="my-40 mx-65">
+        <img src="assets/s3.png" alt="" className="w-full h-auto object-cover" />
+      </div>
+      <Eventdata />
+      <Vetproducts />
       <div className="container mx-auto px-6 mt-20">
         <Vertinarypractice />
       </div>
@@ -52,9 +35,6 @@ const page = () => {
       <div className="container mx-auto px-6 mt-20">
         <Preferredvendors />
       </div>
-      <Footer />
-    </div>
-  )
+    </>
+  );
 }
-
-export default page
